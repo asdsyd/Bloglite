93120d31228156baa4f0274b69865fb1642ca116
Asad Sayeed <ashasd97@gmail.com>
2023-01-14 23:41:03 +0530

    final changes

--- main/routes.py
+++ main/routes.py
@@ -13,8 +13,18 @@ def home():
     posts = Post.query.order_by(Post.date_posted.desc()).paginate(page=page, per_page=6)
     return render_template('home.html', posts=posts)
 
+@main.app_errorhandler(404)
+def error_404(error):
+    return render_template('errors/404.html'), 404
 
-@main.route("/about")
-def about():
-    return render_template('about.html', title='About')
+
+@main.app_errorhandler(403)
+def error_403(error):
+    return render_template('errors/403.html'), 403
+
+
+@main.app_errorhandler(500)
+def error_500(error):
+    return render_template('errors/500.html'), 500
+
 
